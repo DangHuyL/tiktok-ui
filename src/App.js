9c8f40a986a35802c3d03ad495976e8ef1524e00
Dang Huy <nguyendanghuy481@gmail.com>
2022-08-11 17:30:52 +0700

    Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { puplicRoutes } from '~/routes';
+import { DefaultLayout } from '~/combonents/Layout';
+
 function App() {
     return (
-        <div>
-            <h1>kkkkkk</h1>
-        </div>
+        <Router>
+            <div>
+                <Routes>
+                    {puplicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+// Layouts
+import { HeaderOnly } from '~/combonents/Layout';
+
+import Home from '~/Page/Home';
+import Following from '~/Page/Following';
+import Profile from '~/Page/Profile';
+import Upload from '~/Page/Upload';
+import Search from '~/Page/Search';
+
+const puplicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { puplicRoutes, privateRoutes };
