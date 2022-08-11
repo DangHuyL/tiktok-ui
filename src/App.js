514e6ebd3bb2f58a45d58512519c26cdc1a42449
Dang Huy <nguyendanghuy481@gmail.com>
2022-08-11 15:28:27 +0700

    Dựng base dự án

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div>
-      <h1>kkkkkk</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>kkkkkk</h1>
+        </div>
+    );
 }
 
 export default App;
