ab3337ce1ad5be1a5f9f27bd9e96741040522b95
Dang Huy <nguyendanghuy481@gmail.com>
2022-08-14 22:11:17 +0700

    Xây dựng UI phần Header #4

--- src/combonents/Layout/combonents/Header/index.js
+++ src/combonents/Layout/combonents/Header/index.js
@@ -6,6 +6,10 @@ import {
     faSpinner,
     faMagnifyingGlass,
     faPlus,
+    faEllipsisVertical,
+    faEarthAsia,
+    faCircleQuestion,
+    faKeyboard,
 } from '@fortawesome/free-solid-svg-icons';
 
 import Button from '~/combonents/Button';
@@ -14,7 +18,22 @@ import style from './Header.module.scss';
 import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/combonents/Popper';
 import AccountItem from '~/combonents/AccountItem';
-
+import Menu from '~/combonents/Popper/Menu';
+const MENU_ITEM = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcut',
+    },
+];
 const cx = classNames.bind(style);
 
 function Header() {
@@ -73,6 +92,12 @@ function Header() {
                         Up load
                     </Button>
                     <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEM}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
